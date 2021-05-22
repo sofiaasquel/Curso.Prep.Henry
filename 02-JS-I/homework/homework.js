00d5c77b3dd8ac63fc873b6b3b21d67e4fcf811b
sofiaasquel <sofiaasquel.sa@gmail.com>
2021-05-22 16:04:42 -0300

    modificado 02-JS-I/homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -218,7 +218,7 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-  return (base * altura)/2;
+  return base * altura/2
 }
 
 
@@ -226,7 +226,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  return euro * (1,2);
+  return euro * 1.20;
   
 }
 
