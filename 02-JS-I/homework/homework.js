b6339656ad51a3d4a7821cafa52e95677b86e33f
sofiaasquel <sofiaasquel.sa@gmail.com>
2021-05-22 19:30:40 -0300

    modificado 02-JS-I/homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -165,7 +165,7 @@ function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
 
- Math.random (numeroRandom);
+ Math.random (0,1);
 }
 
 function esPositivo(numero) {
